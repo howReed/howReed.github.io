0ab305decd954efeaa549b09c6e76707c6574d83
“howReed” <mikeycooper2@gmail.com>
2025-05-09 23:07:16 +0100

    button styling

--- main.js
+++ main.js
@@ -12,12 +12,23 @@ function removeItem() {
     }
 }
 
+function resetCount() {
+    count = 0;
+    document.getElementById('item-count').innerText = count;
+}
+
 function setLightMode() {
-    document.body.style.backgroundColor = "rgb(255, 255, 255)";
+    document.body.style.backgroundColor = "#ffffff";
     document.body.style.color = "black";
+
+    document.querySelector('.light-mode').classList.add('active');
+    document.querySelector('.dark-mode').classList.remove('active');
 }
 
 function setDarkMode() {
-    document.body.style.backgroundColor = "rgb(40, 40, 40)";
+    document.body.style.backgroundColor = "#212529";
     document.body.style.color = "white";
+
+    document.querySelector('.dark-mode').classList.add('active');
+    document.querySelector('.light-mode').classList.remove('active');
 }
